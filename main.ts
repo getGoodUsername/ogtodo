371e0b2313ea0cdeed2d6982e7896067c4a68a14
Osbaldo Gonzalez Jr. <osbaldogonzalezjr@gmail.com>
2024-10-15 14:10:23 -0700

    Experimenting, still dont have a solid footing of anything

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
-import { Plugin } from 'obsidian';
+import { CachedMetadata, Plugin, TFile } from 'obsidian';
 
 export default class OGTodo extends Plugin {
 	override async onload() {
+		this.registerEvent(this.app.metadataCache.on('changed', (file, data, cache: CachedMetadata) => {
+			const {frontmatter, links} = cache;
+			console.log('frontmatter/yaml header', frontmatter);
+			console.log('links', links);
+
+			frontmatter
+		}))
 	}
+
 }
