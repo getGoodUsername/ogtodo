8adf83d2c7a852994e6697d0cd0aa174e5f14786
Osbaldo Gonzalez Jr. <osbaldogonzalezjr@gmail.com>
2024-10-15 17:04:31 -0700

    know how to make new files now

--- main.ts
+++ main.ts
@@ -1,14 +1,38 @@
-import { CachedMetadata, Plugin, TFile } from 'obsidian';
+import { Plugin, IconName } from 'obsidian';
 
 export default class OGTodo extends Plugin {
+	static readonly #NEW_TODO_ICON: IconName = 'list-plus';
 	override async onload() {
-		this.registerEvent(this.app.metadataCache.on('changed', (file, data, cache: CachedMetadata) => {
-			const {frontmatter, links} = cache;
-			console.log('frontmatter/yaml header', frontmatter);
-			console.log('links', links);
+		// this.registerEvent(this.app.metadataCache.on('changed', (file, data, cache: CachedMetadata) => {
+		// 	const {frontmatter, links} = cache;
+		// 	console.log('frontmatter/yaml header', frontmatter);
+		// 	console.log('links', links);
 
-			frontmatter
-		}))
+		// 	frontmatter
+		// }))
+
+		this.addRibbonIcon(OGTodo.#NEW_TODO_ICON, 'Make new todo', () =>
+		{
+			console.log('making new todo...');
+			this.makeNewTodo();
+		})
+
+		this.addCommand({
+			id: 'make-new-todo',
+			name: 'Make New Todo',
+			icon: OGTodo.#NEW_TODO_ICON,
+			mobileOnly: false,
+			/**
+			 * hotkey (keyboard shortcut) should not make
+			 * multiple new todos when held down
+			 */
+			repeatable: false,
+			callback: () => { console.log('from command, making a new todo!'); this.makeNewTodo(); }
+		})
 	}
 
+	makeNewTodo()
+	{
+		this.app.vault.create(`new_todo ${Date.now()}.md`, 'empty for now...');
+	}
 }
